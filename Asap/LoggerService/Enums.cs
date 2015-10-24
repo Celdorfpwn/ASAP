de078cfcb5a8ec63983e7863bf4bb87404c79290
Valentin Mitaru <valentin.mitaru@softvision.ro>
2015-10-24 12:55:55 +0300

    Added LoggerService

---
+++ Asap/LoggerService/Enums.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoggerService
+{
+    public enum LogType
+    {
+        Info,
+        Warning,
+        Error,
+        Exception
+    }
+}
---
+++ Asap/LoggerService/Log.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoggerService
+{
+    public class Log : ILog
+    {
+        #region Properties
+
+        /// <summary>
+        /// Gets and sets all available log messages
+        /// </summary>
+        public ObservableCollection<LogEntry> Logs { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// The constructor
+        /// </summary>
+        public Log()
+        {
+            Logs = new ObservableCollection<LogEntry>();
+        }
+        
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Add a new log message
+        /// </summary>
+        /// <param name="newLog">New log entry</param>
+        public void AddLog(LogEntry newLog)
+        {
+            Logs.Add(newLog);
+        }
+
+        #endregion
+    }
+}
